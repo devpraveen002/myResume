beca73444768f317d88e4d5fd25d3ec9540b6510
devpraveen002 <dev.praveen002@gmail.com>
2024-04-23 02:49:15 +0530

    finished, want to add icons

--- myResume/src/app/app.component.ts
+++ myResume/src/app/app.component.ts
@@ -3,11 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { InfoComponent } from './info/info.component';
 import { SummaryComponent } from './summary/summary.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoComponent, SummaryComponent,ProjectsComponent],
+  imports: [RouterOutlet, InfoComponent, SummaryComponent,ProjectsComponent,ExperienceComponent,EducationComponent,AchievementsComponent,SkillsComponent,CoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
